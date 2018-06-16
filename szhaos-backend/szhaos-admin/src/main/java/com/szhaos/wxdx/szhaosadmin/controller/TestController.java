c2c9a6a83e359db50ecdbbdf688604501486677c
szhao <546505246@qq.com>
2018-06-16 20:27:12 +0800

    2018616

--- szhaos-backend/szhaos-admin/src/main/java/com/szhaos/wxdx/szhaosadmin/controller/TestController.java
+++ szhaos-backend/szhaos-admin/src/main/java/com/szhaos/wxdx/szhaosadmin/controller/TestController.java
@@ -1,10 +1,22 @@
 package com.szhaos.wxdx.szhaosadmin.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${name}") // git配置文件里的key
+            String name;
+
+    @Value("${user}") // git配置文件里的key
+            String user;
+
+    @RequestMapping(value = "/testconfig")
+    public String testconfig(){
+        return user+":"+name;
+    }
 
     @RequestMapping(value = "/test")
     public String test() {
